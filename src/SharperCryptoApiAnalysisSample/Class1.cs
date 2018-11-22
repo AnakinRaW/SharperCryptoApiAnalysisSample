8544c0ad6859c2c666a8f46e2b9783eba85b8009
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2018-11-22 22:37:43 +0100

    added type tag

---
+++ src/SharperCryptoApiAnalysisSample/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharperCryptoApiAnalysisSample
+{
+    class Class1
+    {
+        Console.WriteLine("Hello Sharper Crypto-API Analysis");
+	}
+}
